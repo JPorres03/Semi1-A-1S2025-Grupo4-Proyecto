aeec7b4f430f436f3ab24d234edc13a0fcd442b0
Sebastian Solares <3017033530101@ingenieria.usac.edu.gt>
2025-03-05 16:25:57 -0600

    feat:adding more apis

--- BACKEND/Typescript/src/controller/books.controller.ts
+++ BACKEND/Typescript/src/controller/books.controller.ts
@@ -18,13 +18,73 @@ export const getBooks = async (req: Request, res: Response) => {
 
 export const searchBook = async(req: Request, res: Response) => {
     try {
-        const { search } = req.params;
+        const { nombre } = req.body;
         const books = await pool.query(
             `SELECT * FROM Libros WHERE titulo ILIKE $1`,
-            [`%${search}%`]
+            [`%${nombre}%`]
         );
         res.status(200).json(books.rows);
         return;
+    }catch(error: any){
+
+    }
+};
+
+export const detailsBooks = async(req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const book = await pool.query(
+            `SELECT * FROM Libros WHERE id = $1`,
+            [id]
+        );
+        if (!book.rows[0]) {
+            res.status(404).json({ message: 'Book not found' });
+            return;
+        }
+        res.status(200).json(book.rows[0]);
+        return;
+    }catch(error: any){
+
+    }
+}
+
+//solo admin
+export const updateBook = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { titulo, autor, genero, descripcion, nombre } = req.body;
+
+        const result = await pool.query(
+            `SELECT rol FROM Usuarios WHERE id = $1`,
+            [id]
+        );
+        
+        const rolUser = result.rows[0]?.rol;
+        
+        if (rolUser !== 'admin') {
+            res.status(403).json({ message: 'Access denied' });
+            return;
+        }
+
+        const resultado = await pool.query(
+            `UPDATE Libros 
+             SET titulo = $1, autor = $2, genero = $3, descripcion = $4
+             WHERE id = $5
+             RETURNING *`,
+            [titulo, autor, genero, descripcion, id]
+        );
+        
+        if (!resultado.rows[0]) {
+            res.status(404).json({ message: 'Book not found' });
+            return;
+        }
+
+
+        res.status(200).json({ 
+            message: 'Book updated successfully', 
+            book: resultado.rows[0]
+        });
+
     }catch(error: any){
 
     }
--- BACKEND/Typescript/src/routes/books.routes.ts
+++ BACKEND/Typescript/src/routes/books.routes.ts
@@ -1,16 +1,13 @@
 import { Router } from 'express';
-import { getBooks } from '../controller/books.controller';
+import { getBooks, searchBook,detailsBooks,updateBook } from '../controller/books.controller';
 
 const router = Router();
 
 // Listar todos los libros (con filtros)
 router.get('/', getBooks);
-
-router.get('/search');
-/* 
-router.get('/:id', getBookDetails);
-router.post('/:id/acquire', acquireBook);
-router.get('/:id/read', getBookReadUrl); */
+router.post('/search',searchBook);
+router.get('/:id', detailsBooks);
+router.put('/update/:id', updateBook);
 
 export default router;
 
